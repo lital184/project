96f705131e278d5b7cb6bf821fd58ed7a068ad63
lital hadad <lital184@gmail.com>
2023-07-24 10:56:35 +0300

    component that loads data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import { CurrentUserLoader } from './CurrentUserLoader';
-import { UserInfo } from './UserInfo';
+import { ProductInfo } from "./ProductInfo";
+import { ResourceLoader } from "./ResourceLoader";
+import { UserInfo } from "./UserInfo";
 
 function App() {
-	return (
-		<CurrentUserLoader>
-			<UserInfo />
-		</CurrentUserLoader>
-	);
+  return (
+    <>
+      <ResourceLoader resourceUrl="/users/123" resourceName="user">
+        <UserInfo />
+      </ResourceLoader>
+      <ResourceLoader resourceUrl="/products/123" resourceName="product">
+        <ProductInfo />
+      </ResourceLoader>
+    </>
+  );
 }
 
 export default App;
--- src/ProductInfo.js
+++ src/ProductInfo.js
@@ -1,13 +1,15 @@
 export const ProductInfo = ({ product }) => {
-	const { name, price, description, rating } = product;
-
-	return (
-		<>
-		<h3>{name}</h3>
-		<p>{price}</p>
-		<h3>Description:</h3>
-		<p>{description}</p>
-		<p>Average Rating: {rating}</p>
-		</>
-	);
-}
+  const { name, price, description, rating } = product || {};
+console.log("product",product)
+  return product ? (
+    <>
+      <h3>{name}</h3>
+      <p>{price}</p>
+      <h3>Description:</h3>
+      <p>{description}</p>
+      <p>Average Rating: {rating}</p>
+    </>
+  ) : 
+    <p>Loading...</p>
+  
+};
